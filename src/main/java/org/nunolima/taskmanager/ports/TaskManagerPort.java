1aa8aae5424d84697a582b445c57a4430121919b
liman <nuno.lima@paddypowerbetfair.com>
2021-03-06 17:30:16 +0000

    add logic to default task manager

--- src/main/java/org/nunolima/taskmanager/ports/TaskManagerPort.java
+++ src/main/java/org/nunolima/taskmanager/ports/TaskManagerPort.java
@@ -1,13 +1,14 @@
 package org.nunolima.taskmanager.ports;
 
 import org.nunolima.taskmanager.domain.Priority;
+import org.nunolima.taskmanager.domain.Process;
 
 import java.util.List;
 
 public interface TaskManagerPort {
-    void add(Process process);
+    void add(Process process) throws IllegalAccessException;
     List<Process> list();
-    void kill(String pid);
+    void kill(String pid) throws IllegalAccessException;
     void killGroup(Priority priority);
     void killAll();
 }
